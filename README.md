1023d31808ed86fe546b0da4fed20a0fa7cdc8a7
speedy2day <speedy2day@gmx.de>
2020-10-23 23:22:31 +0200

    Add script; More description

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # cmi2knx
 Fetch the Control and Monitoring Interface (C.M.I.) data via JSON API and write to Group Addresses on the KNX Bus
 
-This is a little bash script I wrote, to cyclically fetch mainly temperatures, but also pressure and irradiance from the Control and Monitoring Interface (C.M.I.) from the company Technische Alternative. The data is then converted to the KNX compliant DPT 9.x datapoint representation and sent to the KNX Bus using https://github.com/knxd/knxd or more precisely knxtool.
+This is a little bash script I wrote, to cyclically fetch mainly temperatures, but also pressure and irradiance data from the Control and Monitoring Interface (C.M.I.) from the company Technische Alternative. The data is then converted to the KNX compliant DPT 9.x datapoint representation and sent to the KNX Bus using https://github.com/knxd/knxd or more precisely `knxtool groupwrite`.
+
+The JSON API that is used on the C.M.I. side is described here: https://wiki.ta.co.at/C.M.I._JSON-API
+
+Specification of the datapoint types used on the KNX Bus may be found here: [Datapoint Types - KNX Association](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiImZSmz8vsAhWCsaQKHSxTCwgQFjAAegQIBhAC&url=https%3A%2F%2Fwww.knx.org%2FwAssets%2Fdocs%2Fdownloads%2FCertification%2FInterworking-Datapoint-types%2F03_07_02-Datapoint-Types-v02.01.02-AS.pdf&usg=AOvVaw1Sj0MeH30t81UNAIZd51KQ)
+
+The script might be executed by cron every minute without any command line parameter given. At the end of the scipt a modulo on the current clock minute value takes care, so that every minute only one CAN bus member (behind the C.M.I.) is polled. This is a policy given by the C.M.I. JSON API.
---
+++ cmi2knx.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+
+# Beschreibung:
+# Das Skript liest per JSON-API Parameter wie Temperaturen, Volumenströme,
+# Leistungen, etc. vom Control and Monitoring Interface (C.M.I.) der Technischen Alternative
+# und schreibt diese dann per knxtool (knxd) auf Gruppenadressen am KNX-Bus.
+#
+# Abhängigkeiten:   - jq (Command-line JSON Processor)
+#                   - bc (An arbitrary precision calculator language)
+#                   - curl
+#
+# Autor: Sebastian Schnittert (schnittert@gmail.com)
+# Datum: 18.10.2020
+
+# Die IP-Adresse des TA C.M.I.
+CMI_HOST="192.168.188.21"
+# CAN-Bus Knotennummer Ofen-Regler RSM610
+OVEN_NODE="32"
+# CAN-Bus Knotennummer Solar-Regler RSM610
+SOLAR_NODE="42"
+# CAN-Bus Knotennummer CAN-Monitor
+MONITOR_NODE="50"
+# Absoluter Pfad zur Login-Credentials-Datei fürs C.M.I. - Login als "expert" notwendig
+# Login-File Format: machine <ip> login <name> password <pw>
+LOGIN_FILE="/opt/cmi2knx/.cmi-credentials"
+
+# IP-Adresse des knxd-Host ("localhost", wenn gleicher Rechner)
+KNXD_HOST="localhost"
+# Befehl zum Schreiben einer Gruppenadresse
+KNX_GROUPWRITE="knxtool groupwrite ip:${KNXD_HOST}"
+
+
+# Aufbau der Daten vom Ofen-Regler und Definition der entsprechenden Gruppenadresse:
+# Inputs
+#    1 - Temperatur Außenfühler                 - °C
+OVEN_I1_GROUPADDRESS="2/3/0"
+#    2 - Temperatur Wasser im Ofen              - °C
+OVEN_I2_GROUPADDRESS="2/3/1"
+#    3 - Temperatur (nach) Rücklaufanhebung     - °C
+OVEN_I3_GROUPADDRESS="2/3/15"
+#    4 - Temperatur Ofen-Vorlauf                - °C
+OVEN_I4_GROUPADDRESS="2/3/2"
+#    5 - Temperatur Vorlauf Fußbodenheizung     - °C
+OVEN_I5_GROUPADDRESS="2/3/3"
+#    6 - Volumenstrom Ofenpumpe                 - l/h
+OVEN_I6_GROUPADDRESS="2/4/0"
+
+
+# Aufbau der Daten vom Solar-Regler:
+# Inputs
+#    1 - Temperatur Kollektor 1                 - °C
+SOLAR_I1_GROUPADDRESS="2/3/9"
+#    2 - Solarstrahlung Dach                    - W/m²
+SOLAR_I2_GROUPADDRESS="2/2/2"
+#    3 - Temperatur Vorlauf Solar Primär        - °C
+SOLAR_I3_GROUPADDRESS="2/3/11"
+#    4 - Temperatur Rücklauf Solar Sekundär     - °C
+SOLAR_I4_GROUPADDRESS="2/3/14"
+#    5 - Temperatur Vorlauf Solar Sekundär      - °C
+SOLAR_I5_GROUPADDRESS="2/3/13"
+#    6 - Volumenstrom Solar Sekundär            - l/h
+SOLAR_I6_GROUPADDRESS="2/4/1"
+# DL-Bus
+#    1 - Temperatur Speicher Oben               - °C
+SOLAR_D1_GROUPADDRESS="2/3/8"
+#    2 - Temperatur Speicher Oben Mitte         - °C
+SOLAR_D2_GROUPADDRESS="2/3/7"
+#    3 - Temperatur Speicher Mitte              - °C
+SOLAR_D3_GROUPADDRESS="2/3/6"
+#    4 - Temperatur Speicher Unten Mitte        - °C
+SOLAR_D4_GROUPADDRESS="2/3/5"
+#    5 - Temperatur Speicher Unten              - °C
+SOLAR_D5_GROUPADDRESS="2/3/4"
+#    6 - Temperatur Kollektor 2                 - °C
+SOLAR_D6_GROUPADDRESS="2/3/10"
+#    9 - Volumenstrom Solar Primär              - l/h
+SOLAR_D7_GROUPADDRESS="2/4/2"
+#   10 - Temperatur Rücklauf Solar Primär       - °C
+SOLAR_D8_GROUPADDRESS="2/3/12"
+
+
+# Aufbau der Daten vom Monitor:
+# Inputs
+#    1 - Temperatur Technikraum                 - °C
+MONITOR_I1_GROUPADDRESS="2/3/16"
+#    2 - rel. Luftfeuchte Technikraum           - %
+MONITOR_I2_GROUPADDRESS="2/5/0"
+#    3 - Temperatur Taupunkt                    - °C
+MONITOR_I3_GROUPADDRESS="2/3/17"
+#    4 - Luftdruck Technikraum                  - mbar
+MONITOR_I4_GROUPADDRESS="2/6/0"
+
+main() {
+    case $1 in
+    # ===== OFEN-Regler =====
+        "--oven")
+        # Daten vom Ofen-Regler abholen und prüfen
+        get_and_check_json "${OVEN_NODE}" "I"
+
+        # Verarbeitung der Ofen-Input-Daten
+        for i in {1..6}
+        do
+            # Wert mittels JSON-Parser holen und in DPT 9.xxx Format umwandeln
+            HEX=$(dec_to_dpt_9 "$(jq '.Data.Inputs['$((i-1))'].Value.Value' <<< "${JSON}")")
+            # Wert am Bus setzen
+            write_to_knx_grp "${HEX}" "OVEN" "I" ${i}
+        done
+        ;;
+
+    # ===== SOLAR-Regler =====
+        "--solar")
+        # Daten vom Solar-Regler abholen und prüfen
+        get_and_check_json "${SOLAR_NODE}" "I,D"
+
+        # Verarbeitung der Solar-Input-Daten
+        for i in {1..6}
+        do
+            # Wert mittels JSON-Parser holen und in DPT 9.xxx Format umwandeln
+            HEX=$(dec_to_dpt_9 "$(jq '.Data.Inputs['$((i-1))'].Value.Value' <<< "${JSON}")")
+            # Wert am Bus setzen
+            write_to_knx_grp "${HEX}" "SOLAR" "I" ${i}
+        done
+        # Verarbeitung der Solar-DL-Bus-Daten
+        for i in {1..8}
+        do
+            # Wert mittels JSON-Parser holen und in DPT 9.xxx Format umwandeln
+            HEX=$(dec_to_dpt_9 "$(jq '.Data."DL-Bus"['$((i-1))'].Value.Value' <<< "${JSON}")")
+            # Wert am Bus setzen
+            write_to_knx_grp "${HEX}" "SOLAR" "D" ${i}
+        done
+        ;;
+
+    # ===== MONITOR =====
+        "--monitor")
+        # Daten vom Monitor abholen und prüfen
+        get_and_check_json "${MONITOR_NODE}" "I"
+
+        # Verarbeitung der Monitor-Input-Daten
+        for i in {1..4}
+        do
+            # Wert mittels JSON-Parser holen und in DPT 9.xxx Format umwandeln
+            HEX=$(dec_to_dpt_9 "$(jq '.Data.Inputs['$((i-1))'].Value.Value' <<< "${JSON}")")
+            # Wert am Bus setzen
+            write_to_knx_grp "${HEX}" "MONITOR" "I" "${i}"
+        done
+        ;;
+
+    # ===== DEFAULT =====
+        *) 
+        echo "Falscher Eingabeparameter."
+        echo "Nur --oven, --solar oder --monitor sind für die Spezifikation des CAN-Knoten zugelassen"
+    esac
+}
+
+# Funktion zum Abholen und prüfen der JSON-Daten vom C.M.I.
+# $1: CAN-Knotennummer
+# $2: Abzuholende Parameter [I | O | D | ...] (vgl. CMI_JSON_API_V5.pdf)
+get_and_check_json() {
+    # curl URL zusammensetzen
+    local URL="http://${CMI_HOST}/INCLUDE/api.cgi?jsonnode=${1}&jsonparam=${2}"
+    # Daten vom C.M.I abholen
+    JSON=$(curl -s --netrc-file "${LOGIN_FILE}" "${URL}")
+    # Auf Erfolg prüfen
+    if [ "$?" != "0" ]; then
+        echo "ABBRUCH: Abrufen der JSON-Daten vom C.M.I. fehlgeschlagen." & exit 0
+    fi
+    # Prüfen, ob sinvolle Daten vom C.M.I. geliefert wurden.
+    # Werden z.B. mehr als eine Anfrage pro Minute an das C.M.I. gestellt,
+    # wird mit "TOO MANY REQUESTS" geantwortet.
+    if [ "$(jq '.Status' <<< "${JSON}")" != "\"OK\"" ]; then
+        echo "ABBRUCH: Keine validen Daten vom C.M.I. erhalten." & exit 0
+    fi
+}
+
+# Funktion zum Schreiben der Gruppenadressen am KNX-Bus
+# $1: Zu schreibender 2-Byte-HEX-Wert
+# $2: Gruppenadressen-Prefix
+# $3: Parameter-Typ [I | O | D | ...] (vgl. CMI_JSON_API_V5.pdf)
+# $4: Index
+write_to_knx_grp() {
+    # Zu schreibende Gruppenadresse
+    local GRP="\$${2}_${3}${4}_GROUPADDRESS"
+    # Wert am Bus setzen
+    eval "${KNX_GROUPWRITE} ${GRP} ${1} &> /dev/null"
+}
+
+# Funktionsbeschreibung:
+# Der Eingabeparameter in Dezimalschreibweise (z.B. 154.93) wird 
+# in das KNX DPT 9.xxx Format umgewandelt und als 2-Byte Hex zurückgegeben.
+# Binärformat des DPT 9.xxx:    FEEEEMMM MMMMMMMM
+#                               F = 0/1 -> positiver/negativer Wert
+#                               E = Exponent
+#                               M = Mantisse
+# Uwandlung: Dezimalwert = ((1-2*F) * 2^E * M) / 100
+#
+# Funktionsparameter: $1 - Dezimalwert zur Umrechnung in DPT 9.xxx
+#                     $2 - Multiplikationsfaktor (z.B. um Einheiten anzupassen)
+#
+# Abhängigkeiten:   - bc (An arbitrary precision calculator language)
+#
+# Autor: Sebastian Schnittert (schnittert@gmail.com)
+# Datum: 18.10.2020
+dec_to_dpt_9() {
+
+    # Multiplikationsfaktor setzen
+    FACT="1"
+    if [[ $# -gt 1 ]] ; then
+        FACT=$2
+    fi
+
+    # Exponent initialisieren
+    let E=0
+    # Zwei Dezimalstellen mitnehmen
+    M=$(bc <<< "$1*$FACT*100/1")
+
+    # Exponenten bestimmen
+    while [ "$M" -gt 2047 ] || [ "$M" -lt -2047 ]; do
+        M=$(bc <<< "$M/2")
+        let E++
+    done
+
+    # Zweier-Komplement für negative Werte
+    if [ "$M" -lt 0 ]; then
+        M=$(bc <<< "2^12+$M")
+    fi
+        
+    # Binärwerte zusammenbauen
+    EB=$(printf "%4b" "$(bc <<< "obase=2; $E")" | sed 's^ ^0^g')
+    MB=$(printf "%12b" "$(bc <<< "obase=2; $M")" | sed 's^ ^0^g')
+    B=$(cut -c -1 <<< "$MB")${EB}$(cut -c 2- <<< "$MB")
+
+    # Als Hex formatieren
+    H=$(printf "%04X" "$(bc <<< "obase=10;ibase=2; $B")")
+    echo "$(cut -c -2 <<< "$H") $(cut -c 3- <<< "$H")"
+}
+
+# Wenn mit Parameter aufgerufen wird, so weitergeben
+if [[ $# -gt 0 ]] ; then
+    # Main-Funktion mit allen Kommandozeilen-Parametern aufrufen
+    main "$@"
+else
+    # Anhand der Uhrzeit entscheiden, welcher CAN-Teilnehmer abgefragt wird
+    # (Wird nötig, da das C.M.I. nur 1 Anfrage pro Minute zulässt)
+    case $(bc <<< "$(date +%M)%3") in
+        "0")
+        main --oven
+        ;;
+        "1")
+        main --solar
+        ;;
+        "2")
+        main --monitor
+        ;;
+    esac
+fi
